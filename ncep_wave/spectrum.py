19b8fd0261880da247015c5232319b76a50b04e1
Ayal Lutwak <worm60@gmail.com>
2021-03-27 00:12:42 -0700

    spec: Implemented lazy spectrum parsing
    This is a bit of a hack and is really only practical for either
    reading the first record, or iterating through all records. It might
    be fine for what I'm doing, but if I ever need to search for a
    particular time it would be good to make the records property
    searchable, or at least indexible

--- ncep_wave/spectrum.py
+++ ncep_wave/spectrum.py
@@ -40,6 +40,7 @@ class Spectrum:
 
         def __init__(self, rtime, pid, lat, lon, d, UA, UD, crnt, crnt_dir, freqs, dfreqs, dirs):
             self.rtime = rtime
+            self.pid = pid
             self.lat = lat
             self.lon = lon
             self.depth = d      # Depth
@@ -72,51 +73,90 @@ class Spectrum:
     def __init__(self, fspec_path):
         term.message(f"Parsing data in {fspec_path}")
 
-        self._df = None
+        self.fspec = open(fspec_path)
+        self._records = []
+        self._parse_header()
 
-        with open(fspec_path) as fspec:
-            self.parse_header(fspec)
-            self.parse_data(fspec)
+    def location(self):
+        """ Returns the location of the station
+        """
+        if len(self._records) == 0:
+            record = next(self.records)
+        else:
+            record = self._records[0]
+        return record.lat, record.lon
+
+    def station(self):
+        if len(self._records) == 0:
+            record = next(self.records)
+        else:
+            record = self._records[0]
+        return record.pid
 
     @property
-    def df(self):
-        if self._df is None:
-            df = self.freqs[1:]/self.freqs[:-1]
-            self._df = np.append(df, df[-1])
-        return self._df
+    def records(self):
+        self._records = []
+        while True:
+            rec = self._parse_record()
+            if not rec:
+                return
+            self._records.append(rec)
+            yield rec
 
-    def parse_header(self, fspec):
-        line = fspec.readline().strip()
+    def _parse_header(self):
+        """ Parses the header
+
+        This captures:
+        * n freqs
+        * n dirs
+        * n points (grid name is ignored)
+        * Frequencies, and derives df
+        * Directions
+        """
+        # Capture metadata
+        line = self.fspec.readline().strip()
         m = re.fullmatch(r"'WAVEWATCH III SPECTRA' +([0-9]+) +([0-9]+) +([0-9]+).*", line)
         if not m:
             raise ValueError(f"Bad header format: {line}")
         self.nfreqs, self.ndirs, self.npoints = [int(val) for val in m.groups()]
 
+        # Capture frequencies
         freqs = []
-        while indent(fspec) == 1:
-            freqs += [float(val) for val in fspec.readline().split()]
+        while indent(self.fspec) == 1:
+            freqs += [float(val) for val in self.fspec.readline().split()]
         if len(freqs) != self.nfreqs:
             raise ValueError(f"Wrong number of frequencies: {len(freqs)} != {self.nfreqs}\n {freqs}")
         self.freqs = np.array(freqs)
 
+        # Calculate dfreqs
+        df = self.freqs[1:]/self.freqs[:-1]
+        self.df = np.append(df, df[-1])
+
+        # Capture directions
         dirs = []
-        while indent(fspec) == 2:
-            dirs += [float(val) for val in fspec.readline().split()]
+        while indent(self.fspec) == 2:
+            dirs += [float(val) for val in self.fspec.readline().split()]
         if len(dirs) != self.ndirs:
             raise ValueError(f"Wrong number of directions: {len(dirs)} != {self.ndirs}\n{dirs}")
         self.dirs = np.array(dirs)
 
-    def parse_data(self, fspec):
-        self.records = []
-        while True:
-            rec = self.parse_record(fspec)
-            if not rec:
-                break
-            self.records.append(rec)
+    def _parse_record(self):
+        """ Parses a single record
 
-    def parse_record(self, fspec):
+        A record consists of:
+        * The record time YYYYMMDD HH0000
+        * Station name
+        * Station latitude
+        * Station longitude
+        * Water depth
+        * Wind speed (m/s)
+        * Wind direction (deg)
+        * Current speed (m/s)
+        * Current direction (deg)
+        * ndirs*nfreqs data points
+        """
         # Read time
-        timeline = fspec.readline()
+        timeline = self.fspec.readline()
 
         if not timeline:
             return None
@@ -124,17 +164,17 @@ class Spectrum:
         rtime = time.mktime(time.strptime(timeline.strip(), "%Y%m%d %H%M%S"))
 
         # Read Summary
-        rsum = fspec.readline().strip()
-        m = re.fullmatch(r"'([^ ]+) *' +(-?[0-9.]+) *(-[0-9.]+) +([0-9.]+) +([0-9.]+) +([0-9.]+) +([0-9.]+) +([0-9.]+)",
+        rsum = self.fspec.readline().strip()
+        m = re.fullmatch(r"'([^ ]+) *' +(-?[0-9.]+) *(-?[0-9.]+) +([0-9.]+) +([0-9.]+) +([0-9.]+) +([0-9.]+) +([0-9.]+)",
                          rsum)
         if not m:
             raise ValueError(f"Bad record header: {rsum}")
-        params = [float(val) for val in m.groups()]
+        params = [m.groups()[0]] + [float(val) for val in m.groups()[1:]]
         record = Spectrum.Record(rtime, *params, self.freqs, self.df, self.dirs)
 
         data = []
-        while indent(fspec) == 2:
-            data += [float(val) for val in fspec.readline().split()]
+        while indent(self.fspec) == 2:
+            data += [float(val) for val in self.fspec.readline().split()]
 
         if len(data) != self.nfreqs * self.ndirs:
             raise ValueError(f"Received an unexpected amount of data: \n{data}\n\n{len(data)} elements")
