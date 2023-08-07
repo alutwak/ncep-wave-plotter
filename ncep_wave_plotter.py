5aa96c3dc932582d5fb34eb02384a671e73b5cab
Ayal Lutwak <alutwak@protonmail.com>
2023-08-07 12:01:02 -0700

    spec: start implement simple multi-station configuration

--- ncep_wave_plotter.py
+++ ncep_wave_plotter.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import yaml
 import argparse
 
 from ncep_wave.forecast import make_forcast
@@ -9,7 +11,8 @@ import ncep_wave.terminal as term
 def main():
     parser = argparse.ArgumentParser("A tool for producing plots from ncep wave data")
     parser.add_argument("action", choices=["forecast"], help="Plot a forecast")
-    parser.add_argument("station")
+    parser.add_argument("-s", "--station", help="Station to generate plots for")
+    parser.add_argument("-f", "--config", help="Config file with a list of stations to generate plots for")
     parser.add_argument("-o", "--outdir", default=DEFAULT_CACHE, help="Output directory")
 
     args = parser.parse_args()
@@ -17,8 +20,24 @@ def main():
     outdir = os.path.expanduser(args.outdir)
 
     if args.action == "forecast":
+
+        if args.station:
+            stations = [args.station]
+        elif args.config:
+            config_path = os.path.expanduser(args.config)
+            if not os.path.exists(config_path):
+                term.message(f"{args.config} does not exist")
+                sys.exit(1)
+            with open(config_path) as f:
+                stations = yaml.safe_load(f)
+        else:
+            term.message("ERROR: Either a station or a config file must be given")
+            parser.print_help()
+            sys.exit(1)
+
         term.message("Generating forecast")
-        make_forcast(args.station, outdir)
+        for station in stations:
+            make_forcast(station, outdir)
 
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ setup(
     install_requires=[
         "flask",
         "numpy",
-        "matplotlib"
+        "matplotlib",
+        "pyyaml"
     ],
     entry_points={
         "console_scripts": [
